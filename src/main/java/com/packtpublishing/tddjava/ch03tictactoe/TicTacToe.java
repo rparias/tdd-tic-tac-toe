997ff7498403e9117ff7dcc03f8a4a40b5794adb
Ronald Arias <ronald.arias@thoughtworks.com>
2020-07-05 15:04:25 -0500

    Requirement 1 - defining the boundaries and what constitutes an invalid placement to throw RuntimeExceptions

--- src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -1,4 +1,35 @@
 package com.packtpublishing.tddjava.ch03tictactoe;
 
 public class TicTacToe {
+
+    private Character[][] board = {
+            {'0', '0', '0'},
+            {'0', '0', '0'},
+            {'0', '0', '0'}};
+
+    public void play(int xAxis, int yAxis) {
+        checkXAxis(xAxis);
+        checkYAxis(yAxis);
+        setBox(xAxis, yAxis);
+    }
+
+    private void setBox(int xAxis, int yAxis) {
+        if (board[xAxis-1][yAxis-1] != '0') {
+            throw new RuntimeException("Box is occupied");
+        } else {
+            board[xAxis-1][yAxis-1] = 'X';
+        }
+    }
+
+    private void checkYAxis(int yAxis) {
+        if (yAxis < 1 || yAxis > 3) {
+            throw new RuntimeException("Y is outside board");
+        }
+    }
+
+    private void checkXAxis(int xAxis) {
+        if (xAxis < 1 || xAxis > 3) {
+            throw new RuntimeException("X is outside board");
+        }
+    }
 }
