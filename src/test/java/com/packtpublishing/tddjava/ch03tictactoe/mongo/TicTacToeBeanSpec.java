d5361dc8da7fb351157c5b5c48e14a721046de7d
Ronald Arias <ronald.arias@thoughtworks.com>
2020-08-01 14:00:08 -0500

    Adds TicTacToeBean

--- src/test/java/com/packtpublishing/tddjava/ch03tictactoe/mongo/TicTacToeBeanSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch03tictactoe/mongo/TicTacToeBeanSpec.java
@@ -1,2 +1,40 @@
-package com.packtpublishing.tddjava.ch03tictactoe.mongo;public class TicTacToeBeanSpec {
+package com.packtpublishing.tddjava.ch03tictactoe.mongo;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class TicTacToeBeanSpec {
+
+    private TicTacToeBean bean;
+    private final int turn = 17;
+    private final int x = 2;
+    private final int y = 3;
+    private final char player = 'X';
+
+    @Before
+    public void before() {
+        bean = new TicTacToeBean(turn, x, y, player);
+    }
+
+    @Test
+    public void whenInstantiatedThenIdIsStored() {
+        assertThat(bean.getTurn(), is(turn));
+    }
+
+    @Test
+    public void whenInstantiatedThenXIsStored() {
+        assertThat(bean.getX(), is(x));
+    }
+
+    @Test
+    public void whenInstantiatedThenYIsStored() {
+        assertThat(bean.getY(), is(y));
+    }
+
+    @Test
+    public void whenInstantiatedThenPlayerIsStored() {
+        assertThat(bean.getPlayer(), is(player));
+    }
 }
